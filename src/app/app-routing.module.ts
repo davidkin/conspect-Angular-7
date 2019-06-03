8e315ca5a326d209c1324b3c47441c337feb7986
David <davidfloyd322@gmail.com>
2019-06-03 19:17:34 +0300

    app, code refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { JsPageComponent } from './js-page/js-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/js', pathMatch: 'full' },
+  {
+    path: 'js',
+    component: JsPageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,30 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { MaterialModule } from './shared/modules/material.module';
+
 import { MenuComponent } from './components/menu/menu.component';
+import { JsPageComponent } from './js-page/js-page.component';
+import { PopupWindowComponent } from './components/popup-theme-window/popup-window.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LocalStorageService } from './shared/services/localStorage.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    JsPageComponent,
+    PopupWindowComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [LocalStorageService],
+  bootstrap: [AppComponent],
+  entryComponents: [PopupWindowComponent]
 })
 export class AppModule { }
